56284f80ae55bfd552107ce3777c0962d8f13819
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-04 15:21:55 +0530

    Update PostListPage.jsx

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -7,7 +7,7 @@ const PostListPage = () => {
   return (
     <div className="">
       <h1 className="mb-8 text-2xl">Development Blog</h1>
-      <button onClick={()=>setOpen(prev=>!prev)} className="bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden">{open?"Close" :"Filter or Search"}</button>
+      {/* <button onClick={()=>setOpen(prev=>!prev)} className="bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden">{open?"Close" :"Filter or Search"}</button> */}
       <div className="flex flex-col-reverse gap-8 md:flex-row">
         <div className="">
           <PostList />
