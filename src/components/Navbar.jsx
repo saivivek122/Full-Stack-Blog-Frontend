247f4d89c444746c30a2558e8084339bc875eaa6
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2026-02-05 08:27:37 +0530

    Comment out About link in Navbar
    Commented out the About link in the Navbar component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
           <Link to="/">Home</Link>
           <Link to="/posts?sort=trending">Trending</Link>
           {/* <Link to="/posts?sort=trending">Most Popular</Link> */}
-          <Link to="/">About</Link>
+          {/* <Link to="/">About</Link> */}
           <a href="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login 👋
@@ -50,7 +50,7 @@ const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/posts?sort=trending">Trending</Link>
         {/* <Link to="/posts?sort=trending">Most Popular</Link> */}
-        <Link to="/">About</Link>
+        {/* <Link to="/">About</Link> */}
           <SignedOut>
         <Link to="/login">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
